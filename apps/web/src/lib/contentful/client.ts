804cdcbd9e74a4a9075a3e23b9d001fe3969c64b
Anahit <angevorgyan@griddynamics.com>
2026-06-24 14:50:10 +0400

    updated node version in dockerFile

--- apps/web/src/lib/contentful/client.ts
+++ apps/web/src/lib/contentful/client.ts
@@ -1,11 +1,5 @@
 import { createClient } from 'contentful';
 
-if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_TOKEN) {
-  if (process.env.NODE_ENV === 'production') {
-    throw new Error('Missing CONTENTFUL_SPACE_ID or CONTENTFUL_ACCESS_TOKEN');
-  }
-}
-
 export const contentfulClient = createClient({
   space:       process.env.CONTENTFUL_SPACE_ID ?? '',
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? '',
